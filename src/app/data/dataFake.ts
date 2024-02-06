cb17d9f261964ed04d63fc481b756d54d9137e3f
MatheusGarciia <garciiamatheus98@gmail.com>
2024-02-06 10:57:01 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO ANUNCIADO',
+    description: 'marvel anuncia um novo filme do homem de ferro',
+    photoCover:
+      'https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg',
+  },
+  {
+    id: '2',
+    title: 'Nova série anunciada no disney +',
+    description: 'blablabalghjalk',
+    photoCover:
+      'https://1.bp.blogspot.com/-EnmcXKByRfI/X9ddM921WWI/AAAAAAACH3A/Qu7snJEBgiUYdHrce_GI-RTkg9X2c3Q_wCLcBGAsYHQ/s540/series.jpg',
+  },
+];
